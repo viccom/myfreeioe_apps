c798865f01d84efb2462f81b898cf6c2dae69713
viccom <Pa88worddong>
2019-07-09 14:48:26 +0800

    fix some bug

--- HW_UPS2000/userlib/linkcfg.lua
+++ HW_UPS2000/userlib/linkcfg.lua
@@ -1,1 +1,14 @@
-local cfg = {    tpls = {               {  id = "TPL000000109",  name = "tpl1",  ver = 2 },               {  id = "TPL000000110",  name = "tpl2",  ver = 3 },           },   Link_type = "socket",   protocol = "tcp",   serial = { port = "/dev/ttymxc0", baudrate = 115200, data_bits = 8, parity = "NONE", stop_bits = 1, flow_control = "OFF" },   socket = { host = "192.168.174.1", port = 502, nodelay = true },   devs = {       {sn = "xx1", name = "sim1", desc = "modbus sim1", addr="1", tpl="tpl1"},       {sn = "xx2", name = "sim2", desc = "modbus sim2", addr="1", tpl="tpl2"},   }}local vvv = 1return cfg
+local cfg = {    
+        tpls = {               
+            {  id = "TPL000000109",  name = "tpl1",  ver = 2 },              
+            {  id = "TPL000000110",  name = "tpl2",  ver = 3 },           },   
+            Link_type = "socket",   protocol = "tcp",   
+            serial = { port = "/dev/ttymxc0", baudrate = 115200, data_bits = 8, parity = "NONE", stop_bits = 1, flow_control = "OFF" },   
+            socket = { host = "192.168.174.1", port = 502, nodelay = true },   
+            devs = {       
+                {sn = "xx1", name = "sim1", desc = "modbus sim1", addr="1", tpl="tpl1"},       
+                {sn = "xx2", name = "sim2", desc = "modbus sim2", addr="1", tpl="tpl2"},   
+        }
+    }
+
+return cfg
